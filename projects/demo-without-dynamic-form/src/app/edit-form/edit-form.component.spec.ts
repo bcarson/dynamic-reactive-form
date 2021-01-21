da76c5e1d6e77ce7af379b88cd4a3f8236895880
darxx <darx21@gmail.com>
2021-01-21 20:22:50 +0200

    Missing spaces

--- projects/demo-without-dynamic-form/src/app/edit-form/edit-form.component.spec.ts
+++ projects/demo-without-dynamic-form/src/app/edit-form/edit-form.component.spec.ts
@@ -3,7 +3,7 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { EditFormComponent } from './edit-form.component';
 import { AppMaterialModule } from '../app-material.module';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import {ReactiveFormsModule} from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
 
 describe('EditFormComponent', () => {
   let component: EditFormComponent;
